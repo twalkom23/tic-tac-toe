5f19e003052c417ccf31c20bb93910d403ec43d6
Tim Walkom <timwalkom@hotmail.com>
2024-02-15 16:19:59 +1030

    The X and Os now display on screen

--- myScript.js
+++ myScript.js
@@ -90,13 +90,27 @@ function addToLeaderBoard(winner) {
 }
 
 function playGame () {
+
+    const contentX = document.createElement('p');
+    contentX.classList.add('content');
+    contentX.textContent = ('X');
+
+
     let counter = 1; //to keep track of whos turn it is
     topLeft.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareOne = userOneCross;
+            const contentXTopLeft = document.createElement('p');
+            contentXTopLeft.classList.add('contentXTopLeft');
+            contentXTopLeft.textContent = ('X');
+            topLeft.appendChild(contentXTopLeft);
         }
         else {
             squareOne = userTwoNought;
+            const contentOTopLeft = document.createElement('p');
+            contentOTopLeft.classList.add('contentOTopLeft');
+            contentOTopLeft.textContent = ('O');
+            topLeft.appendChild(contentOTopLeft);
         }
         console.log(checkForWinner());
         counter ++;
@@ -104,9 +118,17 @@ function playGame () {
     topMiddle.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareTwo = userOneCross;
+            const contentXTopMiddle = document.createElement('p');
+            contentXTopMiddle.classList.add('contentXTopMiddle');
+            contentXTopMiddle.textContent = ('X');
+            topMiddle.appendChild(contentXTopMiddle);
         }
         else {
             squareTwo = userTwoNought;
+            const contentOTopMiddle = document.createElement('p');
+            contentOTopMiddle.classList.add('contentOTopMiddle');
+            contentOTopMiddle.textContent = ('O');
+            topMiddle.appendChild(contentOTopMiddle);
         }
         console.log(checkForWinner());
         counter ++;
@@ -114,9 +136,17 @@ function playGame () {
     topRight.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareThree = userOneCross;
+            const contentXTopRight = document.createElement('p');
+            contentXTopRight.classList.add('contentXTopRight');
+            contentXTopRight.textContent = ('X');
+            topRight.appendChild(contentXTopRight);
         }
         else {
             squareThree = userTwoNought;
+            const contentOTopRight = document.createElement('p');
+            contentOTopRight.classList.add('contentOTopRight');
+            contentOTopRight.textContent = ('O');
+            topRight.appendChild(contentOTopRight);
         }
         console.log(checkForWinner());
         counter ++;
@@ -124,9 +154,17 @@ function playGame () {
     middleLeft.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareFour = userOneCross;
+            const contentXMiddleLeft = document.createElement('p');
+            contentXMiddleLeft.classList.add('contentXMiddleLeft');
+            contentXMiddleLeft.textContent = ('X');
+            middleLeft.appendChild(contentXMiddleLeft);
         }
         else {
             squareFour = userTwoNought;
+            const contentOMiddleLeft = document.createElement('p');
+            contentOMiddleLeft.classList.add('contentOMiddleLeft');
+            contentOMiddleLeft.textContent = ('O');
+            middleLeft.appendChild(contentOMiddleLeft);
         }
         console.log(checkForWinner());
         counter ++;
@@ -134,9 +172,17 @@ function playGame () {
     middleMiddle.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareFive = userOneCross;
+            const contentXMiddleMiddle = document.createElement('p');
+            contentXMiddleMiddle.classList.add('contentXMiddleMiddle');
+            contentXMiddleMiddle.textContent = ('X');
+            middleMiddle.appendChild(contentXMiddleMiddle);
         }
         else {
             squareFive = userTwoNought;
+            const contentOMiddleMiddle = document.createElement('p');
+            contentOMiddleMiddle.classList.add('contentOMiddleMiddle');
+            contentOMiddleMiddle.textContent = ('O');
+            middleMiddle.appendChild(contentOMiddleMiddle);
         }
         console.log(checkForWinner());
         counter ++;
@@ -144,9 +190,17 @@ function playGame () {
     middleRight.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareSix = userOneCross;
+            const contentXMiddleRight = document.createElement('p');
+            contentXMiddleRight.classList.add('contentXMiddleRight');
+            contentXMiddleRight.textContent = ('X');
+            middleRight.appendChild(contentXMiddleRight);
         }
         else {
             squareSix = userTwoNought;
+            const contentOMiddleRight = document.createElement('p');
+            contentOMiddleRight.classList.add('contentOMiddleRight');
+            contentOMiddleRight.textContent = ('O');
+            middleRight.appendChild(contentOMiddleRight);
         }
         console.log(checkForWinner());
         counter ++;
@@ -154,9 +208,17 @@ function playGame () {
     bottomLeft.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareSeven = userOneCross;
+            const contentXBottomLeft = document.createElement('p');
+            contentXBottomLeft.classList.add('contentXBottomLeft');
+            contentXBottomLeft.textContent = ('X');
+            bottomLeft.appendChild(contentXBottomLeft);
         }
         else {
             squareSeven = userTwoNought;
+            const contentOBottomLeft = document.createElement('p');
+            contentOBottomLeft.classList.add('contentOBottomLeft');
+            contentOBottomLeft.textContent = ('O');
+            bottomLeft.appendChild(contentOBottomLeft);
         }
         console.log(checkForWinner());
         counter ++;
@@ -164,9 +226,17 @@ function playGame () {
     bottomMiddle.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareEight = userOneCross;
+            const contentXBottomMiddle = document.createElement('p');
+            contentXBottomMiddle.classList.add('contentXBottomMiddle');
+            contentXBottomMiddle.textContent = ('X');
+            bottomMiddle.appendChild(contentXBottomMiddle);
         }
         else {
             squareEight = userTwoNought;
+            const contentOBottomMiddle = document.createElement('p');
+            contentOBottomMiddle.classList.add('contentOBottomMiddle');
+            contentOBottomMiddle.textContent = ('O');
+            bottomMiddle.appendChild(contentOBottomMiddle);
         }
         console.log(checkForWinner());
         counter ++;
@@ -174,9 +244,17 @@ function playGame () {
     bottomRight.addEventListener('click', function() {
         if (counter % 2 !== 0) {
             squareNine = userOneCross;
+            const contentXBottomRight = document.createElement('p');
+            contentXBottomRight.classList.add('contentXBottomRight');
+            contentXBottomRight.textContent = ('X');
+            bottomRight.appendChild(contentXBottomRight);
         }
         else {
             squareNine = userTwoNought;
+            const contentOBottomRight = document.createElement('p');
+            contentOBottomRight.classList.add('contentOBottomRight');
+            contentOBottomRight.textContent = ('O');
+            bottomRight.appendChild(contentOBottomRight);
         }
         console.log(checkForWinner());
         counter ++;
